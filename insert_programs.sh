e879bd39695a2a4b00eded607a7668d0a0d0fd24
xinzhiniepan <zxinzhiniepan@gmail.com>
2018-02-27 08:08:15 +0800

    modified insert_programs.sh

--- insert_programs.sh
+++ insert_programs.sh
@@ -3,9 +3,8 @@
 # $1 is path of new program
 if [ ! -d $1 ]; then
     mkdir -p $1/{bin,distfiles,libs,resources,scripts,tools}
-    sudo cp -r $1 /opt/
 fi
 
 if [ ! -d /opt/$1 ]; then
-    sudo cp -r $1 /opt/
+    sudo mkdir -p /opt/$1 && sudo cp -r $1/* /opt/$1/
 fi
