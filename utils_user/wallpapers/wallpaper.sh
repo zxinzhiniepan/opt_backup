2be0bee279b8daf9ee4608900c77eba6eec9678a
xinzhiniepan <zxinzhiniepan@gmail.com>
2018-03-02 09:26:11 +0800

    modify wallpaper.sh

--- utils_user/wallpapers/wallpaper.sh
+++ utils_user/wallpapers/wallpaper.sh
@@ -2,7 +2,7 @@
 
 WALLPAPERDIR=/home/zhanghua/MultiMedia/picture/wallpapers
 while true; do
-	find ${WALLPAPERDIR} -type f \( -name '*.jpg' -o -name '*.png' \) -print0 |
+	find ${WALLPAPERDIR} -type f \( -name '*.jpg' -o -name '*.png' -o -name '*.jpeg' \) -print0 |
 		shuf -n1 -z | xargs -0 feh --bg-fill
-	sleep 5m
+	sleep 2m
 done
